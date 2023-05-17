12ad298eaec33cc3ff516d70dd834aa15ba3ea13
ky-zo <zowczak.kamil@gmail.com>
2023-05-17 20:12:21 +0800

    Layout fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,28 @@
-import { ReactNode } from "react";
-import "@styles/global.css";
+import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
-  description: "Discover and share AI Prompts",
+  description: "Discover & Share AI Prompts",
 };
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient"></div>
-          </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
